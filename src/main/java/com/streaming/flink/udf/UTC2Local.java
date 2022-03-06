d0ede68a9064985d5dbebbb9717f64a8ad278fae
gx <gxlevi123@163.com>
2022-03-06 23:39:45 +0800

    add hello

--- src/main/java/com/streaming/flink/udf/UTC2Local.java
+++ src/main/java/com/streaming/flink/udf/UTC2Local.java
@@ -3,11 +3,13 @@ package com.streaming.flink.udf;
 import org.apache.flink.table.functions.ScalarFunction;
 
 import java.sql.Timestamp;
+import java.time.LocalTime;
 
 public class UTC2Local extends ScalarFunction {
 
     public Timestamp eval(Timestamp s) {
         long timestamp = s.getTime() + 28800000;
+        System.out.println("hello world");
         return new Timestamp(timestamp);
     }
 
