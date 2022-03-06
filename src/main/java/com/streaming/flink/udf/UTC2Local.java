bd48194d05850a3dcbcfddfb07e73847db913af6
gx <gxlevi123@163.com>
2022-03-06 23:41:28 +0800

    add list print

--- src/main/java/com/streaming/flink/udf/UTC2Local.java
+++ src/main/java/com/streaming/flink/udf/UTC2Local.java
@@ -3,12 +3,14 @@ package com.streaming.flink.udf;
 import org.apache.flink.table.functions.ScalarFunction;
 
 import java.sql.Timestamp;
-import java.time.LocalTime;
+import java.util.Arrays;
 
 public class UTC2Local extends ScalarFunction {
 
     public Timestamp eval(Timestamp s) {
         long timestamp = s.getTime() + 28800000;
+        Arrays.asList("1", "2", "3").stream()
+                .forEach(System.out::println);
         System.out.println("hello world");
         return new Timestamp(timestamp);
     }
